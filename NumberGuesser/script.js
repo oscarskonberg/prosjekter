3056b3beb1c162847d837e6bc0f924068a4923b8
Oscar Skønberg <73654020+oscarskonberg@users.noreply.github.com>
2023-10-13 18:08:13 +0200

    Added som extra comments for future development

--- NumberGuesser/script.js
+++ NumberGuesser/script.js
@@ -1,3 +1,16 @@
+// Ekstra oppgaver som kan legges til:
+/*
+
+* You probably calculated the distance from the computer guess to the target and from the human guess to the target. 
+Move this into a separate getAbsoluteDistance() function that takes two numbers and returns the distance, 
+and then use that inside your compareGuesses() function.
+
+* Add functionality to check whether the user guess is between 0 and 9 and alert() the user that their number is out of range. 
+It’s not possible to set a number outside this range with the + and = buttons, 
+but users can do so by typing directly in the input field.
+
+*/
+
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
